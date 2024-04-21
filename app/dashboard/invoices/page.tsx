5a7ce37e47b5fbef0eb6e4776a89181c5fbb8d0c
Ejaz <mohammadejaz978@gmail.com>
2024-04-21 21:42:11 +0530

    UI changed

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <p className="text-center from-amber-100">Invoices Page</p>;
+  }
---
+++ app/ui/Home.tsx
@@ -0,0 +1,6 @@
+export default function Home() {
+  return <main className="flex min-h-screen flex-col p-6">
+    <input type="text"/>
+    <button type="button">Submit</button>
+  </main>;
+}
